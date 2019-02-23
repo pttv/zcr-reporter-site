1b599505664f7f4139ea654b76443c528e99429d
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-23 13:58:55 +0700

    Checkout static/scripts.js to work with html

---
+++ src/static/scripts.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+function parseReportsInput() {
+  try {
+    const inputFile = $('#reports-input')[0].files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = async event => {
+      try {
+        const csvData = String(event.target.result);
+        const records = await ZCR.parseCsv(csvData);
+        $('#output').text(JSON.stringify(records[0]));
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    fileReader.readAsText(inputFile);
+  } catch (error) {
+    alert(error.message);
+  }
+}
