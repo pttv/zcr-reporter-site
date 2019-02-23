bd12262109cbdf121e61ed46e53742ca7f485db5
Ethan Nguyen <thanhnx.605@gmail.com>
2019-02-23 17:20:31 +0700

    Refine scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,26 @@
-function parseReportsInput() {
-  try {
+function parseCsvReports() {
+  return new Promise((resolve, reject) => {
     const inputFile = $('#reports-input')[0].files[0];
     const fileReader = new FileReader();
     fileReader.onload = async event => {
       try {
         const csvData = String(event.target.result);
         const records = await ZCR.parseCsv(csvData);
-        const report = Handlebars.templates.report(records[0]);
-        $('#report-container').html(report);
+        resolve(records);
       } catch (error) {
         alert(error.message);
       }
     };
     fileReader.readAsText(inputFile);
+  })
+}
+
+async function handleInputFiles(params) {
+  try {
+    const records = await parseCsvReports();
+    const report = Handlebars.templates.report(records[0]);
+    $('#report-container').html(report);
   } catch (error) {
-    alert(error.message);
+    alert(JSON.stringify(error));
   }
 }
