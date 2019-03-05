03f51310085c6b94edfc2d26d9460e684a2efb7d
Ethan Nguyen <thanhnx.605@gmail.com>
2019-03-05 13:51:10 +0700

    Remove unused files

--- src/js/fetchers.js
+++ src/js/fetchers.js
@@ -38,11 +38,4 @@ export async function fetchChartImage(record) {
   const imageLink = selector('div.content_wrap_laso > img')[0].attribs.src;
   const imageUrl = new URL(imageLink);
   return imageUrl.origin + imageUrl.pathname;
-
-  // const { data: pageHtml } = await axios.post(CHART_GENERATOR_URL, qs.stringify(body));
-  // const selector = cheerio.load(pageHtml);
-  // const imageLink = selector('div.content_wrap_laso > img')[0].attribs.src;
-  // const { data: imageData } = await axios.get(imageLink, { responseType: 'arraybuffer' });
-  // const imageBase64 = Buffer.alloc(imageData.byteLength, imageData, 'binary').toString('base64');
-  // return `data:image/png;base64,${imageBase64}`;
 }
